6e636114517f225e8f4adce8be11835fa9008d01
Kais Temimi <kassus007@gmail.com>
2020-11-29 12:34:35 -0800

    create component dropDown and feed

---
+++ car-pooling/src/app/feed/feed.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RideService } from '../../services/ride.service';
+import { DriverService} from '../../services/driver.service';
+ 
+
+@Component({
+  selector: 'app-feed',
+  templateUrl: './feed.component.html',
+  styleUrls: ['./feed.component.scss']
+})
+export class FeedComponent implements OnInit {
+
+  @Input() rides: any[];
+  @Input() user: any;
+  @Input() driver: any[];
+  constructor( private router: Router, private rideService: RideService, private driverService: DriverService) {}
+   
+  ngOnInit() {
+
+    //    this.rideService.getAllRides().subscribe(rides=>{
+    //   this.allRides = rides
+    //   console.log(rides)
+     
+    // })
+  }
+  reserve(rideId) {
+    console.log(this.user)
+    this.rideService.reserveRide({rideId: rideId, passengerId: this.user.id}).subscribe(results => {
+    console.log(results);
+    this.router.navigate(['passenger/profile'])
+   
+    })
+  }
+
+//   viewProfile(){
+//  this.driverService.currentDriver = this.rides[0].driver
+//     this.router.navigate(['passenger/profile'])
+//   }
+
+}
+
+
+
+
+
