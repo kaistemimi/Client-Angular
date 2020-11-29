bb00660d4a5078abd824e468693b4e403cc5c498
Kais Temimi <kassus007@gmail.com>
2020-11-29 12:22:42 -0800

    config file angular.json, package.json, routing.module and create component helper

--- car-pooling/src/app/app-routing.module.ts
+++ car-pooling/src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DriverComponent } from './components/driver/driver.component';
+import { PassengerComponent } from './components/passenger/passenger.component';
+import { PassengerProfileComponent } from './components/passenger/passenger-profile/passenger-profile.component';
+import {CarComponent} from './components/driver/car/car.component';
+import { AuthGuard } from './components/_helper/auth.guard';
+import { DriverProfileComponent } from './components/driver/driver-profile/driver-profile.component';
+import { AuthPassengerGuard } from './components/_helper/auth.passenger.guard';
+import { FeedbackComponent } from './components/driver/feedback/feedback.component';
+import {DriverDetailComponent} from './components/passenger/driver-detail/driver-detail.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{path: '', redirectTo: 'home', pathMatch: 'full'},
+{path:'login', component: LoginComponent},
+{path:'register',component: RegisterComponent},
+{path:'contact', component: ContactComponent},
+{path:'home', component: HomeComponent},
+{path :'passenger/detail/:id', component : DriverDetailComponent , canActivate : [AuthPassengerGuard]},
+{path :'car', component: CarComponent , canActivate : [AuthGuard]},
+{path:'driver/profile', component: DriverProfileComponent, canActivate : [AuthGuard]},
+{path: 'driver', component: DriverComponent , canActivate : [AuthGuard] },
+{path:'passenger', component:  PassengerComponent, canActivate : [AuthPassengerGuard]},
+{path:'passenger/profile', component:  PassengerProfileComponent, canActivate : [AuthPassengerGuard]},
+{path:'driver/feedback/:id', component:  FeedbackComponent, canActivate : [AuthGuard]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
